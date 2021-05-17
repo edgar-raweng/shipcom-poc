445917b94ca33739eadf06a54abc2f58cf271bbc
Edgar Yerena <edgar.yerena@raweng.com>
2021-05-17 16:41:46 -0500

    Added windows 10 support

---
+++ windows/shipcom/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::shipcom::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::shipcom::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
