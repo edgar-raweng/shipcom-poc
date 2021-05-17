29dadc64ef562ac9c8febc9cbcf92e2745d47b70
Edgar Yerena <edgar.yerena@raweng.com>
2021-05-17 15:27:39 -0500

    Fresh RN project

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Import core libraries
+import { Text, View } from 'react-native'
+
+const App = () => {
+
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+    </View>
+  )
+
+}
+
+export default App
